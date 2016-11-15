fe0e275cd26176fe0237d59640f689b343b9c8b2
Balasa Sorin Stefan <bsorin.stefan@gmail.com>
2016-11-15 13:24:42 +0200

    Child theme fix

--- versionChecker.js
+++ versionChecker.js
@@ -6,7 +6,7 @@ if( kallyasStyleSheet ){
             if(xhr.status == 200 && xhr.readyState == 4) {
                 var sourceCode = xhr.responseText;
                 // alert('The source code is:-\n'+sourceCode);
-                if( sourceCode.includes('Theme Name: Kallyas') ){
+                if( sourceCode.includes('Text Domain: zn_framework') ){
                     var myRegexp = /Version: (\d+\.\d+.\d+)+/g;
                     var match = myRegexp.exec(sourceCode);
                     document.body.innerHTML += '<div id="znVersionChecker">Kallyas '+match[0]+'</div>';
